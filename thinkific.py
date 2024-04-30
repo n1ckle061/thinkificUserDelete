4a0591be66e8d191e5ad83f9ee37afbe6efb42ac
Trung Le <trung.le@ametek.com>
2024-04-30 14:30:04 -0500

    changed ssl certificate

--- thinkific.py
+++ thinkific.py
@@ -12,7 +12,7 @@ def grab_ids(email_list, header):
     result = []
     for email in email_list:
         params = {"query[email]" : email}
-        response = requests.get('https://api.thinkific.com/api/public/v1/users', params=params, headers=header, verify="certadmin.crt")
+        response = requests.get('https://api.thinkific.com/api/public/v1/users', params=params, headers=header, verify="certadmin1.crt")
         user_id = response.json()["items"][0]["id"]
         result.append(user_id)
     return result
@@ -20,7 +20,7 @@ def grab_ids(email_list, header):
 def delete_users(users_to_delete_list, header):
     for id in users_to_delete_list:
         params = {"id": f"{id}"}
-        response = requests.delete(f"https://api.thinkific.com/api/public/v1/users/{id}", params=params, headers=header, verify="certadmin.crt")
+        response = requests.delete(f"https://api.thinkific.com/api/public/v1/users/{id}", params=params, headers=header, verify="certadmin1.crt")
         code = response.status_code
         if code == 204:  print("User successfully deleted")
         else: print(f"Failed to delete user with id {id}")
